045a31ba2156ffee9cd662f48fe0624c8fb95954
ZRA <roshanmarlia.ashik@cognizant.com>
2019-08-30 21:40:27 +0200

    My first Spring boot web project

---
+++ springbootdemoproject/src/main/java/com/springboot/example/springbootdemoproject/SpringbootdemoprojectApplication.java
@@ -0,0 +1,23 @@
+package com.springboot.example.springbootdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class SpringbootdemoprojectApplication {
+	
+	@RequestMapping("/")
+	public String indexPage() {
+		return "Assalamualaikum wrwb";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoprojectApplication.class, args);
+	}
+
+}
