3ab792c29f849bde7ecfafd5ad44130e6b932f5a
ZRA <roshanmarlia.ashik@cognizant.com>
2019-08-31 22:20:58 +0200

    This is movie info micro service

---
+++ movie-info-service/src/test/java/io/movie/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package io.movie.movieinfoservice;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
