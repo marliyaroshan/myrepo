c3a70f69dab306426d3b5e9114ef38a24c84f886
ZRA <roshanmarlia.ashik@cognizant.com>
2019-08-31 22:21:50 +0200

    This is movie rating by User micro service

---
+++ rating-data-service/src/test/java/io/movie/ratingdataservice/RatingDataServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package io.movie.ratingdataservice;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
