bb995bba4e6c2aacc888c8c95e227a6bdfec2848
ZRA <roshanmarlia.ashik@cognizant.com>
2019-08-31 22:20:17 +0200

    This is movie catalogue micro service

---
+++ movie-catalogue-service/src/main/java/io/movie/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package io.movie.moviecatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
+	}
+
+}
